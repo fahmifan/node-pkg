4fd3af9e6c677d6f53abd8eee2a2d167716f20a3
miun173 <fahmifan173@gmail.com>
2019-04-18 13:31:50 +0700

    feat: setup backend with node-6 & pkg for build

---
+++ index.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+require('babel-register')({
+  presets: ['env']
+});
+
+module.exports = require('./src');
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import co from 'co';
+import express from 'express';
+
+const config = {
+  port: process.env.PORT || '3000',
+}
+
+const app = express();
+
+app.get('/api/ping', (_, res) => res.status(200).json({ message: 'pong' }));
+app.listen(config, () => {
+  console.log(`server listening on http://localhost:${config.port}`);
+});
